2d8c2fe4839013d304ea303751173045810a5468
Olusheyi Gaji <seezie@gmail.com>
2022-06-08 00:22:52 -0400

    I created an uncomplete main file for my assignment

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+fig = ["R", "S", "P"]
+# When the program is run, ask the user to pick an option between "R", "P" or "S"
+while True:
+    for item in fig:
+        if item == "R":
+            print("You chose rock, You rock!")
+        else:
+            item == "S"
+            print("you chose a scissor")
+
+        else
